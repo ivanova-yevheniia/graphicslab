b1f4a6a9829bf347928f339e177bc15f6c58dbff
Yevheniia Ivanova <71208873+ivanova-yevheniia@users.noreply.github.com>
2021-11-01 16:44:24 +0100

    Upload files

---
+++ graphics.py
@@ -0,0 +1,17 @@
+from PIL import Image, ImageDraw
+with open("DS2.txt", "r") as fl:
+    data = fl.readlines()
+    xy = []
+    for line in data:
+        x, y = line.split(" ")
+        xy.append(int(y))
+        xy.append(540-int(x))
+fl.close()
+
+canvas = Image.new("RGB", (960, 540), (255, 255, 255))
+
+dr = ImageDraw.Draw(canvas)
+dr.point(xy, fill=0)
+
+canvas.show()
+canvas.save("IMAGE", "PNG")
